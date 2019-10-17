c1bdde69de00b166d291534aa5dafa23ebea5fc2
Hazim <hazim_malik@hotmail.com>
2019-10-17 11:57:12 +0200

    Create sort-column.js

---
+++ sort-column.js
@@ -0,0 +1,17 @@
+  
+document.addEventListener('DOMContentLoaded', function(event) {
+  var activeTab = document.getElementsByClassName('active')[0];
+  if (activeTab != null) {
+    if (activeTab.innerText == "Recent") {
+      setTimeout(clickSort,1000)
+    }
+  }
+})
+
+function clickSort() {
+  var lastStatusSortArrow = document.evaluate ('//*[@id="projectstatus"]/tbody/tr[1]/th[4]/a/span', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+  
+  while (lastStatusSortArrow.singleNodeValue.innerText.indexOf('↑') == -1 ) {
+    lastStatusSortArrow.singleNodeValue.click();
+  }
+}
